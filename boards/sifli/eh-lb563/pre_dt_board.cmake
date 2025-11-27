b80c73a906419dcf2ca6d5148c628dd370313abd
Yang Hongbo <yanghongbo@iotpi.io>
2025-11-27 21:30:24 +0800

    remove lb563 board defs

--- boards/sifli/eh-lb563/pre_dt_board.cmake
+++
@@ -1,5 +0,0 @@
-# Copyright (c) 2021 Linaro Limited
-# SPDX-License-Identifier: Apache-2.0
-list(APPEND DTS_ROOT ${BOARD_DIR}/../../dts)
-list(APPEND DTS_ROOT ${BOARD_DIR}/../../../drivers)
-
--- boards/sifli/em-lb525/board.cmake
+++
@@ -1,8 +0,0 @@
-# SPDX-License-Identifier: Apache-2.0
-
-# keep first
-board_runner_args(sfrunner "--device=SF32LB52X" "--speed=4000")
-
-# keep first
-board_set_flasher_ifnset(sfrunner)
-board_finalize_runner_args(sfrunner)
