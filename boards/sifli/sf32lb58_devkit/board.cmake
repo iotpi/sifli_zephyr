f8023c030565bd3c0b2d9ed20bcbd04f8cf8119e
Yang Hongbo <yanghongbo@iotpi.io>
2025-11-27 21:41:47 +0800

    updated board.cmake

--- boards/sifli/sf32lb58_devkit/board.cmake
+++ boards/sifli/sf32lb58_devkit/board.cmake
@@ -1,3 +1,3 @@
-board_runner_args(probe-rs "--chip=SF32LB58" "--speed=4000" "--protocol=swd")
+board_runner_args(probe-rs "--chip=SF32LB58" "--speed=10000" "--protocol=swd")
 
 include(${ZEPHYR_BASE}/boards/common/probe-rs.board.cmake)
